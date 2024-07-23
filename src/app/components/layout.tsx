9316985309ad082d9f3bd353762b1b73ba3550af
Stephen <saintobis11@gmail.com>
2024-07-23 19:24:47 +0100

    layout fix

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Header from "./header";
 import Footer from "./footer";
 
-const Layout = ({ children, showHeaderAndFooter = true }) => {
-  return (
-    <div>
-      {showHeaderAndFooter && <Header />}
-        {children}
-      {showHeaderAndFooter && <Footer />}
-    </div>
-  );
+type LayoutProps = {
+    children: ReactNode;
+    showHeaderAndFooter?: boolean; 
+};
+
+const Layout = ({ children, showHeaderAndFooter = true }: LayoutProps) => {
+    return (
+        <div>
+            {showHeaderAndFooter && <Header />}
+            {children}
+            {showHeaderAndFooter && <Footer />}
+        </div>
+    );
 };
 
 export default Layout;
